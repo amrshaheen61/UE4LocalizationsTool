9f9685af92d5c72ebeb23e8859bdb6386c2be407
Amr Shaheen <sha829001@gmail.com>
2022-05-12 19:36:51 +0200

    Update Program.cs

--- UE4localizationsTool/Program.cs
+++ UE4localizationsTool/Program.cs
@@ -15,18 +15,6 @@ namespace UE4localizationsTool
 
         static void Main()
         {
-
-            // Uasset Uasset = new Uasset(@"E:\Games\SIFU\Sifu\Content\Paks\Exports\Sifu\Content\Localization\StringTables\Menu\SkillTree\ST_UI_SkillTree.uasset");
-            //MemoryList memoryList = new MemoryList(@"E:\win10\Downloads\Compressed\FModel\Output\Exports\Sifu\Content\Localization\Dialogues\es\amr");
-            //  MemoryList memoryList = new MemoryList(Uasset.Exports_Directory[0].ExportData);
-            // new Uexp(Uasset);
-            //  Console.WriteLine(Uasset.GetExportPropertyName(Uasset.Exports_Directory[0].ExportClass));
-            //  Console.WriteLine(memoryList.GetSize());
-            //Console.WriteLine(memoryList.GetByteValue(true,1));
-            //Console.WriteLine(memoryList.GetUByteValue());
-
-
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());
